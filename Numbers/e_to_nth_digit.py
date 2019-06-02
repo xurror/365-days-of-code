bda2bacb45f87d5f6fd4b969955e623394be8c13
xurror <kaze.nasser@outlook.com>
2019-06-02 09:08:30 +0100

    compute Euler's Number to a given decimal place

---
+++ Numbers/e_to_nth_digit.py
@@ -0,0 +1,23 @@
+# Funtion to compute Factorial number
+def factorial(n):
+    if n == 1:
+        return 1
+    
+    else:
+        #print(f"call factorial funtion")
+        return n*factorial(n - 1)
+
+def eulersNumber(limit):
+    # Initialise Euler's Number
+    e = 1
+
+    while limit > 0:
+        e += 1/factorial(limit)
+        limit -= 1
+
+    return e
+
+if __name__ == "__main__":
+    number_of_dp = int(input("Enter the number of decimal places: "))        
+
+    print(f"{eulersNumber(200):.{number_of_dp}f}")
