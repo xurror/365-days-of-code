0c941c56589902b9f67f61f6fc0fe908854bb32d
xurror <kaze.nasser@outlook.com>
2019-06-20 20:41:10 +0100

    Fix imports for prime1 and prime2

--- tests/Numbers_test/pi_test.py
+++ tests/Numbers_test/pi_test.py
@@ -1,5 +1,7 @@
 from nose.tools import *
-from projects.Numbers.prime_factorisation import prime
+from projects.Numbers.prime_factorisation import prime1, prime2
 
 def test_dp():
-    assert_equal(prime(7), True)
+
+    assert_equal(prime1(7), True)
+    assert_equal(prime2(7), True)
