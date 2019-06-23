1cd9fa1258d3467e42f445d37e8b859de4cb281c
xurror <kaze.nasser@outlook.com>
2019-06-23 12:43:25 +0100

    build up the views

--- projects/RSS_feed_reader/rss/views.py
+++ projects/RSS_feed_reader/rss/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+import feedparser
+
 # Create your views here.
+
+
+def index(request):
+
+    if request.GET.get('url'):
+        url = request.GET['url'] # Getting URL
+        feed = feedparser.parse(url) # Parsing XML data
+
+    else:
+        feed = None
+
+    return render(request, 'reader.html', {'feed': feed,})
