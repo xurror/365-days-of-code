161108414b4dd30bbf8f57168c49444352d5c287
Yemdjih Kaze Nasser <kaze.nasser@outlook.com>
2019-07-29 09:58:26 +0200

    more fizzes and buzzes

--- projects/Text/Fizz_Buzz.py
+++ projects/Text/Fizz_Buzz.py
@@ -18,6 +18,19 @@ def buzz(i):
 
         return False
 
+def more_fizz_buzz(x, y, z):
+    a, b, c = x, y, z
+
+    for i in range(1,c+1):
+        if i % a == 0 and i % b != 0:
+            print("Fizz")
+        elif i % b == 0 and i % a != 0:
+            print("Buzz")
+        elif i % b == 0 and i % a == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+    
 if __name__ == "__main__":
 
     print("Number\tFizz-Buzz")
